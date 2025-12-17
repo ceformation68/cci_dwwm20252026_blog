16025f4e1a8ab90200653248a5341142c29e8072
Christel <christel.ehrhart@ce-soft.info>
2025-12-17 08:58:07 +0100

    Création d'un compte - partie 2 Avec insertion bdd

--- create_account.php
+++ create_account.php
@@ -9,7 +9,7 @@
 	include("_partial/header.php");
 	
 	// Traitement du formulaire
-	var_dump($_POST);
+	//var_dump($_POST);
 	$strName 		= $_POST['name']??"";
 	$strFirstname 	= $_POST['firstname']??"";
 	$strMail 		= $_POST['mail']??"";
@@ -37,15 +37,29 @@
 		// Si le formulaire est rempli correctement
 		if (count($arrError) == 0){
 			// => Ajout dans la base de données 
-			
 			// 1. Etablir la connexion
-			
+			try{
+				// Connexion à la base de données
+				$db= new PDO(
+								"mysql:host=localhost;dbname=blog_php", // Serveur et BDD
+								"root", //Nom d'utilisateur de la base de données
+								"",// Mot de passe de la base de données
+								array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC) // Mode de renvoi
+				);
+				// Pour résoudre les problèmes d’encodage
+				$db->exec("SET CHARACTER SET utf8");
+				// Configuration des exceptions
+				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);			
+			} catch(PDOException$e) {
+				echo "Échec : " . $e->getMessage();
+			}
 			// 2. Construire la requête
-			
+			$strRq	= "INSERT INTO users (user_name, user_firstname, user_mail, user_pwd)
+						VALUES ('".$strName."', '".$strFirstname."', '".$strMail."', '".$strPwd."')";
 			// 3. Executer la requête
+			$db->exec($strRq);
 			
-			
-			var_dump("tout est ok");
+			//var_dump("tout est ok");
 		}
 	}	
 ?>
