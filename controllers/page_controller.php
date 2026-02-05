9aaad80c63ef95ff9a70b12fc187f2a3be5c7643
Christel <christel.ehrhart@ce-soft.info>
2026-02-05 15:54:19 +0100

    Configuration envoi de mail => A terminer

--- controllers/page_controller.php
+++ controllers/page_controller.php
@@ -1,4 +1,7 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
 	/** 
 	* Le contrôleur des pages
 	* @author Christel
@@ -17,6 +20,38 @@
 		* Page contact 
 		*/
 		public function contact(){
+
+            if (count($_POST)>0){
+                $objMail = new PHPMailer(); // Nouvel objet Mail
+                $objMail->IsSMTP();
+                $objMail->Mailer 		= "smtp";
+                $objMail->CharSet 		= PHPMailer::CHARSET_UTF8;
+
+                // Si on veut afficher les messages de debug
+                $objMail->SMTPDebug  	= 0;
+
+                // Connection au serveur de mail
+                $objMail->SMTPAuth   	= TRUE;
+                $objMail->SMTPSecure 	= "tls";
+                $objMail->Port       	= 587;
+                $objMail->Host       	= "smtp.gmail.com";
+                $objMail->Username 		= 'christel.ceformation@gmail.com';
+                $objMail->Password 		= 'cdbk mrjr aiqo tndi';
+
+                // Comment envoyer le mail
+                $objMail->IsHTML(true); // en HTML
+                $objMail->setFrom('no-reply@blog.fr', 'Mon BLOG'); // Expéditeur
+
+                // Destinataire(s)
+                $objMail->addAddress('contact@ce-formation.com', 'Christel');
+
+                // Mail
+                $objMail->Subject    = "Contact Form";
+                $objMail->Body       = "test mail";
+
+                // Envoyer le mail
+                $objMail->Send();
+            }
 			// Afficher
 			$this->_display("contact");
 		}
