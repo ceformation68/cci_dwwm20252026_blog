31e0943a7463091f84319ec502c728f7dbbda5ed
Christel <christel.ehrhart@ce-soft.info>
2026-02-26 14:20:25 +0100

    Configuration des mails par les variables d'environnement

--- controllers/mother_controller.php
+++ controllers/mother_controller.php
@@ -91,28 +91,26 @@
 		protected function _sendMail(){
 			//$this->_objMail 			= new PHPMailer(); // Nouvel objet Mail
 			$this->_objMail->IsSMTP();
-			$this->_objMail->Mailer 	= "smtp";
+			$this->_objMail->Mailer 	= $_ENV['MAIL_MAILER'];
 			$this->_objMail->CharSet	= PHPMailer::CHARSET_UTF8;
 
 			// Si on veut afficher les messages de debug
 			$this->_objMail->SMTPDebug  = 0;
 
 			// Connection au serveur de mail
-			$this->_objMail->SMTPAuth   	= TRUE;
-			$this->_objMail->SMTPSecure 	= "tls";
-			$this->_objMail->Port       	= 587;
-			$this->_objMail->Host       	= "smtp.gmail.com";
-			$this->_objMail->Username 		= 'christel.ceformation@gmail.com';
-			$this->_objMail->Password 		= 'cdbk mrjr aiqo tndi';
+			$this->_objMail->SMTPAuth   	= $_ENV['MAIL_SMTPAuth'];
+			$this->_objMail->SMTPSecure 	= $_ENV['MAIL_SMTPSecure'];
+			$this->_objMail->Port       	= $_ENV['MAIL_PORT'];
+			$this->_objMail->Host       	= $_ENV['MAIL_HOST'];
+			$this->_objMail->Username 		= $_ENV['MAIL_USERNAME'];
+			$this->_objMail->Password 		= $_ENV['MAIL_PWD'];
 
 			// Comment envoyer le mail
-			$this->_objMail->IsHTML(true); // en HTML
-			$this->_objMail->setFrom('no-reply@blog.fr', 'Mon BLOG'); // Expéditeur
+			$this->_objMail->IsHTML($_ENV['MAIL_ISHTML']); // en HTML
+			$this->_objMail->setFrom($_ENV['MAIL_FROM_EMAIL'], $_ENV['MAIL_FROM_NAME']); // Expéditeur
 
 			// Envoyer le mail
 			$this->_objMail->Send();
 		}
-
-
 
 	}
